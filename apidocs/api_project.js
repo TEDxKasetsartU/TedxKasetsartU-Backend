9e27df75bb743512becbabf24143195c2cd3dd46
Kamontat Chantrachirathumrong <kamontat_c@hotmail.com>
2018-01-17 02:50:52 +0700

    :memo: Ignore docs

--- apidocs/api_project.js
+++
@@ -1,1 +0,0 @@
-define({  "title": "API Documents for TEDxKasetsartU",  "name": "tedxku-backend",  "version": "0.1.0",  "description": "backend api for tedxku server",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-01-16T19:47:59.339Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
