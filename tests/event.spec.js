5939fb23b50c32f49880c90ca630e411442e9916
Kamontat Chantrachirathumrong <kamontat_c@hotmail.com>
2018-01-31 14:32:35 +0700

    :hammer: Clean test code

--- tests/event.spec.js
+++ tests/event.spec.js
@@ -11,13 +11,10 @@ function getRandomInt(min, max) {
 }
 
 function randomEvent() {
-    return settings.model.event.count().then(res => {
-        console.log("event size: " + res);
-        return settings.model.event.randomOne();
-    });
+    return settings.model.event.randomOne();
 }
 
-process.env.PORT = getRandomInt(8000, 8100);
+process.env.PORT = getRandomInt(4002, 19999);
 
 const settings = require("../app/settings");
 
@@ -55,7 +52,7 @@ describe("Events", () => {
         it("it should GET all the events", () => {
             return chai.request(this.server).get("/api/v2/events")
                 .then((res) => {
-                    console.log(res.body);
+                    // console.log(res.body);
                     expect(res.body.complete).to.be.true;
                     expect(res).to.have.status(200);
                 });
@@ -66,11 +63,9 @@ describe("Events", () => {
                 .then((event) => {
                     return chai.request(this.server).get("/api/v2/event/" + event.id);
                 }).then((res) => {
-                    console.log(res.body);
+                    // console.log(res.body);
                     expect(res.body.complete).to.be.true;
                     expect(res).to.have.status(200);
-                }).catch(err => {
-                    console.error(err);
                 });
         });
 
@@ -87,7 +82,7 @@ describe("Events", () => {
         it("it should GET list all year available", () => {
             return chai.request(this.server).get("/api/v2/events/years")
                 .then((res) => {
-                    console.log(res.body);
+                    // console.log(res.body);
                     expect(res.body.complete).to.be.true;
                     expect(res.body.result).to.be.an("array").that.not.empty;
                     expect(res).to.have.status(200);
@@ -98,7 +93,7 @@ describe("Events", () => {
             return randomEvent().then((event) => {
                 return chai.request(this.server).get("/api/v2/events/" + event.year);
             }).then((res) => {
-                console.log(res.body);
+                // console.log(res.body);
                 expect(res.body.complete).to.be.true;
                 expect(res.body.result).to.be.an("array").that.not.be.empty;
                 expect(res).to.have.status(200);
