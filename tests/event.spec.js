53a5d8fce3b599bf48849446c6c12ff66d0b0f42
Kamontat Chantrachirathumrong <kamontat_c@hotmail.com>
2018-01-31 18:34:18 +0700

    :hammer: Add handle error

--- tests/event.spec.js
+++ tests/event.spec.js
@@ -53,10 +53,12 @@ describe("Events", () => {
     describe("/GET event", () => {
         it("it should GET all the events", () => {
             return chai.request(this.server).get("/api/v2/events")
-                .then((res) => {
+                .then(res => {
                     // console.log(res.body);
                     expect(res.body.complete).to.be.true;
                     expect(res).to.have.status(200);
+                }).catch(err => {
+                    console.error(err);
                 });
         });
 
@@ -64,10 +66,12 @@ describe("Events", () => {
             return randomEvent()
                 .then((event) => {
                     return chai.request(this.server).get("/api/v2/event/" + event.id);
-                }).then((res) => {
+                }).then(res => {
                     // console.log(res.body);
                     expect(res.body.complete).to.be.true;
                     expect(res).to.have.status(200);
+                }).catch(err => {
+                    console.error(err);
                 });
         });
 
@@ -83,18 +87,20 @@ describe("Events", () => {
     describe("/GET event year", () => {
         it("it should GET list all year available", () => {
             return chai.request(this.server).get("/api/v2/events/years")
-                .then((res) => {
+                .then(res => {
                     // console.log(res.body);
                     expect(res.body.complete).to.be.true;
                     expect(res.body.result).to.be.an("array").that.not.empty;
                     expect(res).to.have.status(200);
+                }).catch(err => {
+                    console.error(err);
                 });
         });
 
         it("it should GET filter only input year", () => {
             return randomEvent().then((event) => {
                 return chai.request(this.server).get("/api/v2/events/" + event.year);
-            }).then((res) => {
+            }).then(res => {
                 // console.log(res.body);
                 expect(res.body.complete).to.be.true;
                 expect(res.body.result).to.be.an("array").that.not.be.empty;
@@ -107,7 +113,7 @@ describe("Events", () => {
         it("it shouldn't GET not exist year", () => {
             return chai.request(this.server)
                 .get("/api/v2/events/1")
-                .catch((err) => {
+                .catch(err => {
                     const res = err.response;
                     expect(res.body.complete).to.be.false;
                     expect(res.body.message).to.have.own.property("code");
